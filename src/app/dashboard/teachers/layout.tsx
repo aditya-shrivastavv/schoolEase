fdc33e3175ac0c5721d06ddb2e45729723b55b42
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-06-23 14:39:18 +0530

    teachers page layout correction

--- src/app/dashboard/teachers/layout.tsx
+++ src/app/dashboard/teachers/layout.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { Divider, Flex, Heading, Button, Box } from '@chakra-ui/react'
 import React from 'react'
 
 type Props = {
@@ -5,7 +8,22 @@ type Props = {
 }
 
 const TeachersLayout = ({ children }: Props) => {
-  return <>{children}</>
+  return (
+    <Box>
+      <Divider />
+      <Flex justify={'space-between'} align={'center'} my={5}>
+        <Box>
+          <Heading size="lg" fontWeight={'light'}>
+            Teachers Section
+          </Heading>
+        </Box>
+        <Button variant={'outline'} rounded={'full'}>
+          Add Teacher
+        </Button>
+      </Flex>
+      {children}
+    </Box>
+  )
 }
 
 export default TeachersLayout
--- src/app/dashboard/teachers/page.tsx
+++ src/app/dashboard/teachers/page.tsx
@@ -1,26 +1,11 @@
 'use client'
 
-import { Divider, Flex, Heading, Button, Box } from '@chakra-ui/react'
 import React from 'react'
 
 type Props = {}
 
 const TeachersPage = (props: Props) => {
-  return (
-    <Box>
-      <Divider colorScheme="twitter" />
-      <Flex justify={'space-between'} align={'center'} my={5}>
-        <Box>
-          <Heading size="lg" fontWeight={'light'}>
-            Teachers Section
-          </Heading>
-        </Box>
-        <Button variant={'outline'} rounded={'full'}>
-          Add Teacher
-        </Button>
-      </Flex>
-    </Box>
-  )
+  return <div>Teachers Page</div>
 }
 
 export default TeachersPage
