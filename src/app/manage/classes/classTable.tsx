035f9fb7f5feb3befff441c0d7acace1603af941
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-06-30 20:12:39 +0530

    made classes table

--- src/app/manage/classes/classTable.tsx
+++ src/app/manage/classes/classTable.tsx
@@ -1,11 +1,89 @@
 'use client'
 
 import React from 'react'
+import { ThemeProvider } from '@mui/material'
+import {
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridToolbarColumnsButton,
+  GridToolbarContainer,
+  GridToolbarDensitySelector,
+  GridToolbarExport,
+  GridToolbarFilterButton,
+} from '@mui/x-data-grid'
+import { classData } from '@/db/sample'
+import { MuiTheme } from '@/theme/mui'
+import { Button, useToast } from '@chakra-ui/react'
+import { DeleteIcon } from '@chakra-ui/icons'
+import { multipleRowEditToastWarn } from '@/components/toast/toast'
 
-type Props = {}
+let selectedRows: typeof classData = []
 
-const ClassTable = (props: Props) => {
-  return <div>ClassTable</div>
+export default function ClassTable() {
+  const rows = classData
+  const columns: GridColDef[] = [
+    {
+      field: 'name',
+      headerName: 'Class',
+      minWidth: 350,
+      flex: 1,
+    },
+  ]
+
+  return (
+    <div style={{ width: '100%' }}>
+      <ThemeProvider theme={MuiTheme}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          density="compact"
+          checkboxSelection
+          autoHeight
+          onRowSelectionModelChange={(ids) => {
+            const selectedIDs = new Set(ids)
+            selectedRows = rows.filter((row) => selectedIDs.has(row.id))
+            console.log(selectedRows)
+          }}
+          initialState={{
+            pagination: { paginationModel: { pageSize: 10 } },
+          }}
+          pageSizeOptions={[10, 25, 50, 100]}
+          slots={{
+            toolbar: CustomToolbar,
+          }}
+        />
+      </ThemeProvider>
+    </div>
+  )
 }
 
-export default ClassTable
+function CustomToolbar() {
+  const toast = useToast()
+  function changeMyName() {
+    if (selectedRows.length !== 1) {
+      multipleRowEditToastWarn(toast)
+    } else {
+      // do something
+    }
+  }
+
+  return (
+    <GridToolbarContainer sx={{ gap: 1, mb: 1 }}>
+      <GridToolbarDensitySelector />
+      <GridToolbarColumnsButton />
+      <GridToolbarFilterButton />
+      <GridToolbarExport />
+      <Button
+        leftIcon={<DeleteIcon boxSize={'16px'} />}
+        variant={'ghost'}
+        color={'red.500'}
+        borderRadius={'md'}
+        p={'4px 5px'}
+        _hover={{ color: 'red.400', bgColor: 'red.50' }}
+      >
+        DELETE
+      </Button>
+    </GridToolbarContainer>
+  )
+}
