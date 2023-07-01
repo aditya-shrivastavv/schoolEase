a939aa2e86e6aa46d34fb9616de0881b07a49fc9
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-07-01 23:32:12 +0530

    added createClass fn and getAllClasses fn from db

--- src/actions/classActions.ts
+++ src/actions/classActions.ts
@@ -5,12 +5,24 @@ import getRandomColor from '@/lib/color/getRandomColor'
 
 const xata = getXataClient()
 
-export async function createClass(data: { name: string }): Promise<Classes> {
-  const model = {
+/**
+ * Create new class in database.
+ * @param {string} data
+ * @returns
+ */
+export async function createClass(data: { name: string }) {
+  const model: Omit<Classes, 'id'> = {
     name: data.name,
+    class: data.name.split('-')[0],
     section: data.name.split('-')[1],
     colorCode: getRandomColor(),
   }
   const record = xata.db.classes.create(model)
   return record
+}
+
+export async function getAllClasses() {
+  const records = xata.db.classes.select(['id', 'name']).getAll()
+  return JSON.parse(JSON.stringify(records))
+  // Only plain objects can be passed to Client Components from Server Components. Classes or other objects with methods are not supported.
 }
